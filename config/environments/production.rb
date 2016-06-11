e1eb27239d56f38f6a194c65995c0989a8867ba1
Pritish Sai <Pritish.sai@gmail.com>
2016-06-12 04:53:41 +0530

    changes to production.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'aquamyst.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'aquamystapp.herokuapp.com' }
 
 ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
