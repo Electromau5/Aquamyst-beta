3217da83dd750c7c036a74a84f56992d24fa7a8c
Pritish Sai <Pritish.sai@gmail.com>
2016-06-15 00:00:22 +0530

    analytics

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :applicants, only: [:new, :create]
   get 'rainsofcastamere', to: 'applicants#index'
   get 'thankyou', to: 'pages#thankyou'
+  get 'about', to: 'pages#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
