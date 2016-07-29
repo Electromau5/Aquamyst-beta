c297fcd5d38255a7aa6c46d3bd03cd5da9bbb01e
Pritish Sai <Pritish.sai@gmail.com>
2016-07-30 02:05:13 +0530

    Logo test

--- app/assets/javascripts/listing.js
+++ app/assets/javascripts/listing.js
@@ -4,12 +4,10 @@ $(window).load(function(){
 
 
 $(document).ready(function(){
-  $('#grid').imagesLoaded(function(){
   $('#grid').masonry({
           itemSelector: '#listing-box',
           isFitWidth: true,
           isAnimated: !Modernizr.csstransitions
-        });
   });
 });
 
